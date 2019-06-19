b3cf428a0c4e3fa9bc382dee174ddd989bf020c7
Davide Sensi <davide.sen97@gmail.com>
2019-06-19 16:41:50 +0200

    PrimoCommitDavide
    Aggiunta service e fotografo.html

---
+++ authtest-thymeleaf/src/main/java/it/uniroma3/authtest/services/AlbumService.java
@@ -0,0 +1,29 @@
+package it.uniroma3.authtest.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.uniroma3.authtest.model.Album;
+import it.uniroma3.authtest.storage.AlbumRepository;
+
+
+public class AlbumService {
+	@Autowired
+	private AlbumRepository albumRepository;
+	
+	@Transactional
+	public Album inserisci(Album fotografo) {
+		return albumRepository.save(fotografo);
+	}
+	
+	@Transactional
+	public List<Album> tutti(){
+		return (List<Album>) albumRepository.findAll();
+	}
+	
+	public Album albumPerId(Long id) {
+		return this.albumRepository.findById(id).get();
+	}
+}
---
+++ authtest-thymeleaf/src/main/java/it/uniroma3/authtest/services/FotografiaService.java
@@ -0,0 +1,15 @@
+package it.uniroma3.authtest.services;
+
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+import it.uniroma3.authtest.storage.FotografiaRepository;
+
+@Service
+public class FotografiaService {
+
+	
+	public FotografiaService(FotografiaRepository repository, ResourceLoader resourceLoader) {
+		
+	}
+}
